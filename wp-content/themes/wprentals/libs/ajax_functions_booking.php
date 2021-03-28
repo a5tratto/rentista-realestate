4b2600c7b8ad800464d69fa6a23f7a6a0877b275
A5TRATTO TEAM <a5trattoteam@gmail.com>
2021-03-28 16:52:37 +0200

    Update comment giorgio replace $depozit_show > $service_fee_show ricevuta

--- wp-content/themes/wprentals/libs/ajax_functions_booking.php
+++ wp-content/themes/wprentals/libs/ajax_functions_booking.php
@@ -2048,7 +2048,7 @@ if( !function_exists('wpestate_create_invoice_form') ):
                                 <span class="inv_legend"><strong>'.esc_html__( 'Guest Pays','wprentals').'</strong></span>
                                 <span class="inv_data" id="total_amm" data-total="'.esc_attr($total_price_comp).'">'.$total_price_show.'</span>
 
-                                <span class="total_inv_span"><span class="inv_legend invoice_reseration_fee_req"> '.esc_html__( 'Reservation Fee Required','wprentals').':</span> <span id="inv_depozit" data-value="'.esc_attr($depozit).'">'.$service_fee_show.'</span><!-- giorgio replace $depozit_show > $service_fee_show-->
+                                <span class="total_inv_span"><span class="inv_legend invoice_reseration_fee_req">dove siamo '.esc_html__( 'Reservation Fee Required','wprentals').':</span> <span id="inv_depozit" data-value="'.esc_attr($depozit).'">'.$service_fee_show.'</span><!-- giorgio replace $depozit_show > $service_fee_show-->
                                 <div style="width:100%"></div>
                                 <span class="inv_legend invoice_balance_owed">'.esc_html__( 'Balance Owed','wprentals').':</span> <span id="inv_balance" data-val="'.esc_attr($balance).'">'.$balance_show.'</span>
                             </div>';
